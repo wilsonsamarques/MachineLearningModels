ce5dbe9c48496b2a37a7bb336257946c8eb40e3e
wilsonsamarques <wilsonsamarques@gmail.com>
2022-07-29 14:53:04 -0300

    template knn class

---
+++ KNN/knn.py
@@ -0,0 +1,16 @@
+# A sample is classified by the popularity vote of its nearest neighbors
+
+# Eucledian distance
+
+
+class KNN:
+
+    def __init__(self, k=3):
+        self.k = k
+    
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+    
